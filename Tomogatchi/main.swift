039ab57b642ed9e54446f04dfb80f013cfde3ca7
Archie Gherendi <Gherendi.A@etoncollege.org.uk>
2023-11-24 17:14:18 +0000

    Tomogatchi class created, next step to create a tamagotchi and use the class

--- Tomogatchi/main.swift
+++ Tomogatchi/main.swift
@@ -7,5 +7,30 @@
 
 import Foundation
 
-print("Hello, World!")
+class Tamagotchi {
+    
+    private let name: String
+    private var healthMeter: Int
+    private var age: Float
+    private var duck: Bool
+    private var hungerMeter: Int
+    private var happyMeter: Int
+    
+    init(name: String, healthMeter : Int, age: Float, duck : Bool, hungerMeter: Int, happyMeter: Int) {
+        
+        self.name = "Jeffrey"
+        self.healthMeter = 100
+        self.age = 12
+        self.duck = false
+        self.hungerMeter = 100
+        self.happyMeter = 100
+    }
+    
+    func getname() -> String {
+        return "\(name)"
+    }
+}
+
+
+
 
